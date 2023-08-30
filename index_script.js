501522da60fb12aa209e7fa00bce31a78bbf65c9
FTacke <143613082+FTacke@users.noreply.github.com>
2023-08-30 18:06:33 +0200

    Update index_script.js

--- index_script.js
+++ index_script.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
   const mp3LinksContainer = document.getElementById('mp3Links');
   const totalWordCountElement = document.getElementById('totalWordCount');
   const totalDurationElement = document.getElementById('totalDuration');
-  const subfolder = 'logo'; // Unterordner mit Slash am Ende
+  const subfolder = 'logo/'; // Unterordner mit Slash am Ende
 
   let totalWordCount = 0;
   let totalDurationInSeconds = 0;
 
   // Hier generieren wir Links für alle .mp3-Dateien im Unterordner
-  fetch(`${subfolder}`)
+  fetch(`logo/`)
     .then(response => response.text())
     .then(text => {
       const parser = new DOMParser();
